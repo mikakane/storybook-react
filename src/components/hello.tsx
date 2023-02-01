736794d783306482dccab611ab4ea99f8aa325dc
goto <goto@example.com>
2023-02-01 23:17:10 +0900

    update

---
+++ src/components/hello.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Button = ({ props }) => {
+    return (
+      <button
+        type="button"
+        {...props}
+      >
+        hello
+      </button>
+    );
+  };
+  
